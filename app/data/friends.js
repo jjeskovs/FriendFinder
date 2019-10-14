d4d44334335c424a16257a530fc589e0df16c107
jegors jeskovs <jegorsj@gmail.com>
2019-10-13 22:05:40 -0600

    work in progress

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var bestFriends = [
+    {
+        make: "Ducati Panigale V4 S", 
+        photo: "../public/assets/images/DucatiPanigale.jpg", 
+        scores: [5, 5, 5, 1, 1, 1, 1, 1, 1, 1],
+    },
+    {
+        make: "2018 Kawasaki Ninja 400",
+        photo: "../public/assets/images/KawasakiNinja400.jpg",
+        scores: [1, 1, 1, 5, 3, 1, 1, 1, 1, 1]
+    },
+    {
+        make: "2018 Triumph Street Triple R",
+        photo: "HM10/FriendFinder/app/public/assets/images/TriumphStreetTriple.jpg",
+        scores: [3, 3, 3, 5, 4, 1, 1, 1, 1, 1]
+    },
+    {
+        make: "2018 Kawasaki Ninja H2 SX SE",
+        photo: "../public/assets/images/KawasakiNinjaH2 .jpg",
+        scores: [4, 5, 4, 4, 4, 1, 1, 1, 1, 1] 
+    },   
+    {
+        make: "2018 Aprilia Tuono V4 1100 RR",
+        photo: "../public/assets/images/ApriliaTuonoV4 .jpg",
+        scores: [4, 4, 4, 4, 5, 1, 1, 1, 1, 1]
+    },
+    {
+        make: "2018 KTM 1090 Adventure R",
+        photo: "../public/assets/images/KTM1090AdventureR.jpg",
+        scores: [3, 3, 1, 3,1 ,4 ,4, 4, 4, 4]
+    },
+    {
+        make: "2018 Husqvarna TE 250i",
+        photo: "../public/assets/images/HusqvarnaTE250i.jpg",
+        scores: [2, 2, 1,3 , 1, 5, 5, 5, 5, 5]
+    }
+];
+
+module.exports = bestFriends;
