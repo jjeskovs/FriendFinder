c9517ab43719783af54d9a796d87d210687867bd
jegors jeskovs <jegorsj@gmail.com>
2019-10-14 23:54:28 -0600

    building modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,13 +7,46 @@ module.exports = function (app) {
         res.json(bikes);
  
     });
-    app.post("/api/friends", function(req, res) {
-        console.log(req.body.scores);
-        
-        // for (i = 0; i < )
-
-        res.end();
-    });
     
-}
+    app.post("/api/friends", function(req, res) {
+        
+        // console.log(req.body.scores);
+      
+        //lets grab the data sent to us 
+        var user = req.body;
+
+        // lets turn user info into what we can use
+        for (var i = 0; i < user.scores.length; i++){
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+        
+        // console.log(user)
+
+        var bestBikeIndex = 0;
+        var minimumDifference = 40;
+
+        for (var i = 0; i < bikes.length; i++){
+            var totalDifference = 0;
+            
+                for (var j = 0; j < bikes[i].scores.length; j++){
+                     var difference = Math.abs(user.scores[j] - bikes[i].scores[j]);
+                        totalDifference += difference;
+                };
+                    if (totalDifference < minimumDifference){
+                        bestBikeIndex = [i];
+                        minimumDifference = totalDifference;
+                    };
+        };
+        res.json(bikes[bestBikeIndex]);
+        // console.log(bikes)
+        // console.log(bestBikeIndex)
+        console.log(bikes[bestBikeIndex])
+        
+    });
+};
+
+        
+ 
+
+    
 
